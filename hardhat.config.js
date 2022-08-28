3d2544a41e743b44b3ca3851f77a1b60be06949f
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2022-08-28 14:44:16 -0700

    configed mumbai

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,16 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.STAGING_INFURA_URL,
+      accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+
+  }
 };
